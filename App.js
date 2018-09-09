ca5bcfaa0017865b23f8a4a23540809f038fdc23
Julio <julio@beautyhall.com.br>
2018-09-09 03:51:19 -0300

    Continuando com o componente Flag

--- App.js
+++ App.js
@@ -30,7 +30,7 @@ export default class App extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
+	justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
   },
