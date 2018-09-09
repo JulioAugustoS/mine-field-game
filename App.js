287a2779aae81988f8224f71fb36cc32122a3d08
Julio <julio@beautyhall.com.br>
2018-09-09 03:14:16 -0300

    Componente Field

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react'
 import {Platform, StyleSheet, Text, View} from 'react-native'
 import params from './src/params'
+import Field from './src/componentes/Field'
 
 export default class App extends Component {
   render() {
@@ -9,6 +10,13 @@ export default class App extends Component {
 			<Text>Iniciando o Mines!!!</Text>
 			<Text>Tamanho da grade:
 				{params.getRowsAmount()}x{params.getColumnsAmount()}</Text>
+
+			<Field />	
+			<Field opened />
+			<Field opened nearMines={1} />
+			<Field opened nearMines={2} />
+			<Field opened nearMines={3} />
+			<Field opened nearMines={6} />
       	</View>
     )
   }
