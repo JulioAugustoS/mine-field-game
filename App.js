a54d4ece9ce11c761223f8ef40dfb6bd80f8b035
Julio Augusto <julioaugusto@admkanabismini2.localdomain>
2018-10-10 18:19:50 -0300

    Modificações para Build

--- App.js
+++ App.js
@@ -48,7 +48,7 @@ export default class App extends Component {
 
 		if(lost){
 			showMines(board)
-			Alert.alert('Perdeeeeeu', 'Seu Animal')
+			Alert.alert('Perdeeeeeu', 'Tente novamente')
 		}
 
 		if(won){
